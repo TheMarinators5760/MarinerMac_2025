35536234f590d898dafb8975b2d4260db43fe66f
The Marinators <themarinators5760@gmail.com>
2025-02-26 22:30:19 -0800

    Update CANDriveSubsystem.java
    everything...

--- subsystems/CANDriveSubsystem.java
+++ subsystems/CANDriveSubsystem.java
@@ -1,8 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -11,35 +9,45 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.DriveCommand;
 
 // Class to drive the robot over CAN
 public class CANDriveSubsystem extends SubsystemBase {
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
+
+  private final SparkMax m_leftLeader;
+  private final SparkMax m_leftFollower;
+  private final SparkMax m_rightLeader;
+  private final SparkMax m_rightFollower;
 
   private final DifferentialDrive drive;
 
   public CANDriveSubsystem() {
     // create brushed motors for drive
-    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+    m_leftLeader = new SparkMax(DriveConstants.leftLeader, MotorType.kBrushed);
+    m_leftFollower = new SparkMax(DriveConstants.leftFollower, MotorType.kBrushed);
+    m_rightLeader = new SparkMax(DriveConstants.rightLeader,MotorType.kBrushed);
+    m_rightFollower = new SparkMax(DriveConstants.rightFollower, MotorType.kBrushed);
+
+   
 
     // set up differential drive class
-    drive = new DifferentialDrive(leftLeader, rightLeader);
+    drive = new DifferentialDrive(m_leftLeader, m_rightLeader);
 
     // Set can timeout. Because this project only sets parameters once on
     // construction, the timeout can be long without blocking robot operation. Code
     // which sets or gets parameters during operation may need a shorter timeout.
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
+
+    
+    m_leftLeader.setCANTimeout(250);
+    m_rightLeader.setCANTimeout(250);
+    m_leftFollower.setCANTimeout(250);
+    m_rightFollower.setCANTimeout(250);
 
     // Create the configuration to apply to motors. Voltage compensation
     // helps the robot perform more similarly on different
@@ -53,26 +61,24 @@ public class CANDriveSubsystem extends SubsystemBase {
     // Set configuration to follow leader and then apply it to corresponding
     // follower. Resetting in case a new controller is swapped
     // in and persisting in case of a controller reset due to breaker trip
-    config.follow(leftLeader);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    config.follow(rightLeader);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    config.follow(m_leftLeader);
+    m_leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    config.follow(m_rightLeader);
+    m_rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    // Remove following, then apply config to right leader
-    config.disableFollowerMode();
-    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Remove following, then apply config to right leade
+   config.disableFollowerMode();
+    m_rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     // Set conifg to inverted and then apply to left leader. Set Left side inverted
     // so that postive values drive both sides forward
     config.inverted(true);
-    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   @Override
   public void periodic() {
   }
 
-  // sets the speed of the drive motors
-  public void driveArcade(double xSpeed, double zRotation) {
-    drive.arcadeDrive(xSpeed, zRotation);
-  }
+// driveSubsystem.driveTank(-xSpeed.getAsDouble(), zRotation.getAsDouble());
+
 }
