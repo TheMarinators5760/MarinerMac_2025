25de8ca1af285a4dbdf2a5578f49c9465fc0dc2b
The Marinators <themarinators5760@gmail.com>
2025-02-26 22:31:45 -0800

    Update AutoCommand.java

--- commands/AutoCommand.java
+++ commands/AutoCommand.java
@@ -38,14 +38,15 @@ public class AutoCommand extends Command {
   @Override
   public void execute() {
     // drive at 1/2 speed
-    driveSubsystem.driveArcade(0.5, 0.0);
+    driveSubsystem.(-0.5, 0.0);
+    
   }
 
   // Runs each time the command ends via isFinished or being interrupted.
   @Override
   public void end(boolean isInterrupted) {
     // stop drive motors
-    driveSubsystem.driveArcade(0.0, 0.0);
+    driveSubsystem.driveTank(0.0, 0.0);
   }
 
   // Runs every cycle while the command is scheduled to check if the command is
