fc8fd5cf2ab324d59772e9f80091cce7f3dc4c33
The Marinators <themarinators5760@gmail.com>
2025-02-26 22:33:29 -0800

    Update Robot.java

--- important/Robot.java
+++ important/Robot.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+/// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -9,14 +9,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 
 import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
@@ -36,107 +35,70 @@ import edu.wpi.first.wpilibj.Timer;
  * steering and an Xbox controller.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftLeaderMotor = new PWMSparkMax(2);
-  private final PWMSparkMax m_leftFollowerMotor = new PWMSparkMax(1);
-  private final PWMSparkMax m_rightLeaderMotor = new PWMSparkMax(4);
-  private final PWMSparkMax m_rightFollowerMotor = new PWMSparkMax(3);
-
-  private final DifferentialDrive m_robotDrive =
-      new DifferentialDrive(m_leftLeaderMotor::set, m_rightLeaderMotor::set);
+  /** Called once at the beginning of the robot program. */
+  
   private final XboxController m_driverController = new XboxController(0);
   private final Timer m_timer = new Timer();
-  /** Called once at the beginning of the robot program. */
-  public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftLeaderMotor);
-    SendableRegistry.addChild(m_robotDrive, m_leftFollowerMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightLeaderMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightFollowerMotor); 
 
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_leftLeaderMotor.setInverted(true);
-    m_leftFollowerMotor.setInverted(true);
-  }
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
 
-  @Override
-  public void teleopPeriodic() {
-    // Drive with tank drive.
-    // That means that the Y axis of the left stick moves the left side
-    // of the robot forward and backward, and the Y axis of the right stick
-    // moves the right side of the robot forward and backward.
-    m_robotDrive.tankDrive(m_driverController.getLeftY(), m_driverController.getRightY());
+
+  private Spark driveLeftLeadSpark = new Spark(2);
+  private Spark driveRightLeadSpark = new Spark(3);
+  private Encoder leftLeadEncoder = new Encoder(0,1);
+  private Encoder rightLeadEncoder = new Encoder(2, 3);
+  private final double kDriveEncoderTick2Meter = 1.0 / 4096 * 0.128 * Math.PI;
+
+
+  public double getDriveEncoderMeters(){
+    return (leftLeadEncoder.get() + -rightLeadEncoder.get()) / 2 * kDriveEncoderTick2Meter;
   }
 
   @Override
   public void robotInit() {
-    
-
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-        // and put our
-        // autonomous chooser on the dashboard.
-        RobotContainer m_RobotContainer = new RobotContainer();
-
-    //ps4controller = new PS4Controller(1);
-
-    //m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-
-    // Used to track usage of the KitBot code, please do not remove
-    HAL.report(tResourceType.kResourceType_Framework, 9);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items
-   * like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
+
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {
-    m_robotDrive.stopMotor();
-  }
-
+  public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {
-  }
+  public void disabledPeriodic() {}
 
-  /**
-   * This autonomous runs the autonomous command selected by your
-   * {@link RobotContainer} class.
-   */
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-  m_timer.restart();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
-      m_robotDrive.tankDrive(-0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
-    }
-    }
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
@@ -144,9 +106,15 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    //if (m_autonomousCommand != null) {
-   // m_autonomousCommand.cancel();
-  // }
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    
   }
 
   @Override
@@ -157,16 +125,6 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {
-  }
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
+  public void testPeriodic() {}
 }
+
