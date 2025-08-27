023c8ddc4a6e0714a1ca8509fe88e55117ff1836
The Marinators <themarinators5760@gmail.com>
2025-08-27 10:40:53 -0700

    Update Robot.java

--- important/Robot.java
+++ important/Robot.java
@@ -1,130 +1,111 @@
-/// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.hal.FRCNetComm.tResourceType;
-
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase;
-
-
-import edu.wpi.first.wpilibj.Timer;
-
 /**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with tank
- * steering and an Xbox controller.
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
-  /** Called once at the beginning of the robot program. */
-  
-  private final XboxController m_driverController = new XboxController(0);
-  private final Timer m_timer = new Timer();
+  private String m_autoSelected;
+  private RobotContainer container = new RobotContainer();
 
-  private Command m_autonomousCommand;
-  private RobotContainer m_robotContainer;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-
-  private Spark driveLeftLeadSpark = new Spark(2);
-  private Spark driveRightLeadSpark = new Spark(3);
-  private Encoder leftLeadEncoder = new Encoder(0,1);
-  private Encoder rightLeadEncoder = new Encoder(2, 3);
-  private final double kDriveEncoderTick2Meter = 1.0 / 4096 * 0.128 * Math.PI;
-
-
-  public double getDriveEncoderMeters(){
-    return (leftLeadEncoder.get() + -rightLeadEncoder.get()) / 2 * kDriveEncoderTick2Meter;
-  }
-
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    // m_chooser.setDefaultOption("Default Auto", AutonomousConstants.KDEFAULT_AUTONOMOUS);
+    // m_chooser.addOption("My Auto", AutonomousConstants.KCUSTOM_AUTONMOUS);
+    // SmartDashboard.putData("Auto choices", m_chooser);
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
    *
    * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    // m_autoSelected = m_chooser.getSelected();
+    // System.out.println("Auto selected: " + m_autoSelected);
+    // switch (m_autoSelected) {
+    //   case AutonomousConstants.KCUSTOM_AUTONMOUS:
+    //     new Autonomous().moveforward(drivetrain).schedule();
+    //     break;
+    //   case AutonomousConstants.KDEFAULT_AUTONOMOUS:
+    //   default:
+    //     // Put default auto code here
+    //     break;
+    // }
+    container.getAutonomousCommand().schedule();
   }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
+  /** This function is called periodically during autonomous. 
+   * Not the instructions of an auto but what runs during it.
+   * For example, direction diagnostics
+  */
 
   @Override
+  public void autonomousPeriodic() {
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
   }
 
+  /** This function is called once when the robot is disabled. */
   @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
-}
 
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
